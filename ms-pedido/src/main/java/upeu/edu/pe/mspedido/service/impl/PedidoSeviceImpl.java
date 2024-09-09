1772a56811b3e38a9c77df815de67243c1984797
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-09 13:39:08 -0500

    a

--- ms-pedido/src/main/java/upeu/edu/pe/mspedido/service/impl/PedidoSeviceImpl.java
+++ ms-pedido/src/main/java/upeu/edu/pe/mspedido/service/impl/PedidoSeviceImpl.java
@@ -1,6 +1,8 @@
 package upeu.edu.pe.mspedido.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import upeu.edu.pe.mspedido.dto.Cliente;
 import upeu.edu.pe.mspedido.dto.Producto;
@@ -28,6 +30,33 @@ public class PedidoSeviceImpl implements PedidoService {
 
     @Override
     public Pedido guardarPedido(Pedido pedido) {
+        // Verificar si el cliente existe
+        Cliente cliente = clienteFeign.listarClienteDtoPorId(pedido.getClienteId()).getBody();
+        if (cliente == null) {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe el cliente");
+        }
+
+        // Verificar si cada producto en los detalles existe
+        List<PedidoDetalle> pedidoDetalles = pedido.getDetalle().stream().map(pedidoDetalle -> {
+            Producto producto = productoFeign.listarProductoDtoPorId(pedidoDetalle.getProductoId()).getBody();
+            if (producto == null) {
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe el producto"); // Alternativamente puedes usar ResponseEntity aquí también
+            }
+            pedidoDetalle.setProducto(producto);
+            return pedidoDetalle;
+        }).collect(Collectors.toList());
+
+        // Asignar los detalles actualizados
+        pedido.setDetalle(pedidoDetalles);
+
+        // Asignar el cliente al pedido
+        pedido.setCliente(cliente);
+
+        ResponseEntity.status(HttpStatus.CREATED).body("Pedido creado exitosamente");
+
+        // Guardar el pedido si todas las validaciones pasaron
+        pedidoRepository.save(pedido);
+
         return pedidoRepository.save(pedido);
     }
 
