fa8db26933c90c8e274ef674e9867b0063867624
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2024-09-09 14:01:14 -0500

    a

--- ms-pedido/src/main/java/upeu/edu/pe/mspedido/controller/PedidoController.java
+++ ms-pedido/src/main/java/upeu/edu/pe/mspedido/controller/PedidoController.java
@@ -1,22 +1,60 @@
 package upeu.edu.pe.mspedido.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import upeu.edu.pe.mspedido.dto.Cliente;
+import upeu.edu.pe.mspedido.dto.Producto;
 import upeu.edu.pe.mspedido.entity.Pedido;
+import upeu.edu.pe.mspedido.entity.PedidoDetalle;
+import upeu.edu.pe.mspedido.feign.ClienteFeign;
+import upeu.edu.pe.mspedido.feign.ProductoFeign;
+import upeu.edu.pe.mspedido.repository.PedidoRepository;
 import upeu.edu.pe.mspedido.service.PedidoService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/pedido")
 public class PedidoController {
     @Autowired
     private PedidoService pedidoService;
+    @Autowired
+    private ClienteFeign clienteFeign;
+    @Autowired
+    private ProductoFeign productoFeign;
 
     @PostMapping
-    public ResponseEntity<Pedido> guardarPedidoResponseEntity(@RequestBody Pedido pedido){
-        return ResponseEntity.ok(pedidoService.guardarPedido(pedido));
+    public ResponseEntity<?> guardarPedidoResponseEntity(@RequestBody Pedido pedido) {
+        // Verificar si el cliente existe
+        Cliente cliente = clienteFeign.listarClienteDtoPorId(pedido.getClienteId()).getBody();
+        if (cliente == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe el cliente");
+        }
+
+        // Verificar si cada producto en los detalles existe
+        List<PedidoDetalle> pedidoDetalles = pedido.getDetalle().stream().map(pedidoDetalle -> {
+            Producto producto = productoFeign.listarProductoDtoPorId(pedidoDetalle.getProductoId()).getBody();
+            if (producto == null) {
+                throw new RuntimeException("No existe el producto con su categoría");
+            }
+            pedidoDetalle.setProducto(producto);
+            return pedidoDetalle;
+        }).collect(Collectors.toList());
+
+        // Asignar los detalles actualizados
+        pedido.setDetalle(pedidoDetalles);
+
+        // Asignar el cliente al pedido
+        pedido.setCliente(cliente);
+
+        // Guardar el pedido si todas las validaciones pasaron
+        Pedido pedidoGuardado = pedidoService.guardarPedido(pedido);
+
+        // Retornar respuesta exitosa
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoGuardado);
     }
 
     @GetMapping
